6181579f3b34727e7ffd850bd376ead93779be56
Luciana Duarte <lucianaduoli@gmail.com>
2025-03-15 03:37:06 +0000

    Programaria

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-if(document.getElementById("nome") estiver preenchido && campo de email estiver preenchido){
-  envie os dados
-}else{
-  peça a usuária que preencha os campos nome && email
+document.getElementById("botaoEnviar").addEventListener ("click", validaFormulário)
+
+
+function validaFormulário(){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById(telefone).value != ""){
+    
+    alert ("Prontinho! Você receberá as novidades por email.")
+  }else{
+    alert ("Por favor, preencha os campos 'Nome', 'E-mail' e 'Telefone'.")
+  }
 }
+
